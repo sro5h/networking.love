54f83ea6b0ed9ded27b37b5e540dd167b673bffa
Paul Meffle <meffle.paul@me.com>
2017-02-23 18:25:18 +0100

    Initial setup

---
+++ main.lua
@@ -0,0 +1,26 @@
+--
+-- # networking.love
+-- Basic networking tests using the [Love2d](http://love2d.org) framework
+--
+-- **Version:** 0.0.1
+-- **License:** MIT
+-- **Source:**  [Github](https://github.com/sro5h/networking.love)
+--
+
+-- ## love.load
+--
+function love.load()
+
+end
+
+-- ## love.update
+--
+function love.update(dt)
+
+end
+
+-- ## love.draw
+--
+function love.draw()
+
+end
